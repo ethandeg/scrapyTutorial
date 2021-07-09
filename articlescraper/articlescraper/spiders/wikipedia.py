d75d341bc00946305945740be0b55c6528e10231
Ethan Degenhardt <ethandeg1996@gmail.com>
2021-07-09 00:10:21 -0500

    wikipedia doesn't crawl right, maybe robots.txt?

---
+++ articlescraper/articlescraper/spiders/wikipedia.py
@@ -0,0 +1,15 @@
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from articlescraper.items import Article
+class WikipediaSpider(CrawlSpider):
+    name = 'wikipedia'
+    allowed_domains = ['en.wikipedia.org']
+    start_urls = ['https://en.wikipedia.org/wiki/Kevin_Bacon']
+    rules = [Rule(LinkExtractor(allow=r'wiki/((?!:),)*$'), callback='parse_info', follow=True)]
+    def parse_info(self, response):
+        article = Article()
+        article['title'] = response.xpath('//h1/text()').get() or response.xpath('//h1/i/text()').get(),
+        article['url']= response.url,
+        article['last_edited']= response.xpath('//li[@id="footer-info-lastmod]/text()').get()
+        return article
